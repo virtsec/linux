86d945367a4586a6e227924aba8bef5ff2f345fd
Sergej Proskurin <proskurin@sec.in.tum.de>
2020-05-01 12:14:02 -0400

    xMP: Add Xen hypercalls and initialize xMP structures
    Co-authored-by: Christopher Roemheld <roemheld@in.tum.de>
    Co-authored-by: Marius Momeu <momeu@sec.in.tum.de>

---
+++ arch/x86/xen/xmp.c
@@ -0,0 +1,366 @@
+#include <linux/mm.h>
+#include <linux/errno.h>
+#include <linux/random.h>
+#include <linux/bootmem.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/smp.h>
+#include <linux/types.h>
+#include <linux/siphash.h>
+#include <linux/bitmap.h>
+#include <linux/topology.h>
+#include <linux/percpu-defs.h>
+
+#include <xen/interface/xmp.h>
+#include <xen/interface/xen.h>
+#include <xen/interface/altp2m.h>
+
+#include <asm/bitops.h>
+#include <asm/unaligned.h>
+#include <asm/fpu/api.h>
+#include <asm/processor-flags.h>
+#include <asm/sections.h>
+
+#include <asm/xen/page.h>
+
+DECLARE_BITMAP(xmp_pdomain_bitmap, XMP_MAX_PDOMAINS);
+DEFINE_SPINLOCK(xmp_pdomain_bitmap_lock);
+
+/*
+ * Pointers to xMP siphash key
+ *
+ * Each view is assigned axMP siphash key which is only visible to the specific
+ * view it was allocated for. Thus, we need a page for each key to reside in.
+ */
+static siphash_key_t *xmp_keys[XMP_MAX_PDOMAINS] = { 0 };
+
+/*
+ * xMP vCPU information structure
+ */
+static struct xmp_vcpu_info xmp_vcpu_info = { 0 };
+
+/*
+ * xMP utilities
+ */
+
+struct xmp_vcpu *xmp_vcpu_ptr(unsigned int cpu)
+{
+        if (cpu >= xmp_vcpu_info.num_vcpus)
+                return NULL;
+
+        return &xmp_vcpu_info.vcpus[cpu];
+}
+
+struct vea_struct *xmp_vcpu_vea(unsigned int cpu)
+{
+        struct xmp_vcpu *vcpu = xmp_vcpu_ptr(cpu);
+
+        return (vcpu ? vcpu->vea : NULL);
+}
+
+struct xmp_vcpu *xmp_current_vcpu(void)
+{
+        unsigned int cpu = smp_processor_id();
+
+        return xmp_vcpu_ptr(cpu);
+}
+
+struct vea_struct *xmp_current_vea(void)
+{
+        unsigned int cpu = smp_processor_id();
+
+        return xmp_vcpu_vea(cpu);
+}
+
+/*
+ * xMP primitives
+ */
+
+static uint8_t __xmp_vmfunc(uint16_t pdomain)
+{
+        uint8_t ret;
+
+        asm volatile("vmfunc;"
+                     : "=a"(ret)
+                     : "a"(XMP_VMFUNC_EPTP_SWITCHING), "c"(pdomain)
+                     : "cc");
+
+        return ret;
+}
+
+uint8_t xmp_vmfunc(uint16_t pdomain)
+{
+        struct xmp_vcpu *vcpu = xmp_current_vcpu();
+
+        /*
+         * This branch prediction only misses during the early stage
+         * when VMFUNC has not been enabled on the current vCPU.
+         */
+
+        if (unlikely(vcpu->status != XMP_VCPU_STATUS_UP))
+                return 0;
+
+        return __xmp_vmfunc(pdomain);
+}
+EXPORT_SYMBOL(xmp_vmfunc);
+
+static uint16_t xmp_create_pdomain(void)
+{
+	uint16_t altp2m_id = 0;
+
+	spin_lock(&xmp_pdomain_bitmap_lock);
+
+	altp2m_id = find_first_zero_bit(xmp_pdomain_bitmap, XMP_MAX_PDOMAINS);
+	if (altp2m_id == XMP_MAX_PDOMAINS) {
+		spin_unlock(&xmp_pdomain_bitmap_lock);
+		return XMP_INVALID_PDOMAIN;
+	}
+
+	bitmap_set(xmp_pdomain_bitmap, altp2m_id, 1);
+
+	spin_unlock(&xmp_pdomain_bitmap_lock);
+
+	return altp2m_id;
+}
+
+static void xmp_destroy_pdomain(uint16_t altp2m_id)
+{
+	int set;
+
+	spin_lock(&xmp_pdomain_bitmap_lock);
+	set = test_and_clear_bit(altp2m_id, xmp_pdomain_bitmap);
+	spin_unlock(&xmp_pdomain_bitmap_lock);
+
+	if (!set)
+		return;
+
+	altp2m_destroy_view(altp2m_id);
+}
+
+int xmp_isolate_pages(uint16_t altp2m_id, void *addr, unsigned long num_pages,
+	xenmem_access_t access)
+{
+	int ret;
+	unsigned long i, j;
+	xen_pfn_t gfn;
+
+	for (i = 0; i < num_pages; i++) {
+		gfn = virt_to_pfn(addr) + i;
+
+		ret = altp2m_isolate_pdomain(altp2m_id, gfn, XENMEM_access_n, access);
+		if (ret)
+			return -EFAULT;
+
+		for (j = 1; j < (XMP_MAX_PDOMAINS - 1); j++) {
+			ret = altp2m_set_suppress_ve(j, gfn, false);
+			if (ret) {
+				xmp_pr_info("Could not clear suppress_be bit for GFN = %lu", gfn);
+				return -EFAULT;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int xmp_isolate_page(uint16_t altp2m_id, void *addr, xenmem_access_t priv_access)
+{
+	return xmp_isolate_pages(altp2m_id, addr, 1, priv_access);
+}
+EXPORT_SYMBOL(xmp_isolate_page);
+
+uint16_t xmp_alloc_pdomain(void)
+{
+	uint16_t altp2m_id;
+
+	altp2m_id = xmp_create_pdomain();
+	if (altp2m_id == XMP_INVALID_PDOMAIN)
+		return XMP_INVALID_PDOMAIN;
+
+	xmp_keys[altp2m_id] = (void *)get_zeroed_page(GFP_KERNEL);
+	if (!xmp_keys[altp2m_id])
+		goto xmp_early_alloc_destroy_pdomain;
+
+	/* Generate xMP siphash key for this view */
+	get_random_bytes(xmp_keys[altp2m_id], sizeof(siphash_key_t));
+
+	/* Isolate xMP key for this view */
+	if (xmp_isolate_page(altp2m_id, xmp_keys[altp2m_id], XENMEM_access_r))
+		goto xmp_early_alloc_free_key;
+
+	xmp_pr_info("Created early pdomain %u", altp2m_id);
+
+	return altp2m_id;
+
+xmp_early_alloc_free_key:
+	free_page((unsigned long)xmp_keys[altp2m_id]);
+
+xmp_early_alloc_destroy_pdomain:
+	xmp_destroy_pdomain(altp2m_id);
+
+	return XMP_INVALID_PDOMAIN;
+}
+EXPORT_SYMBOL(xmp_alloc_pdomain);
+
+/*
+ * Initialization
+ */
+
+static uint16_t __init xmp_early_alloc_pdomain(void)
+{
+	uint16_t altp2m_id;
+
+	altp2m_id = xmp_create_pdomain();
+	if (altp2m_id == XMP_INVALID_PDOMAIN)
+		return XMP_INVALID_PDOMAIN;
+
+	xmp_keys[altp2m_id] = memblock_virt_alloc_node(PAGE_SIZE, NUMA_NO_NODE);
+	if (!xmp_keys[altp2m_id])
+		goto xmp_early_alloc_destroy_pdomain;
+
+	/* Generate xMP siphash key for this view */
+	get_random_bytes(xmp_keys[altp2m_id], sizeof(siphash_key_t));
+
+	/* Isolate xMP key for this view */
+	if (xmp_isolate_page(altp2m_id, xmp_keys[altp2m_id], XENMEM_access_r))
+		goto xmp_early_alloc_free_key;
+
+	xmp_pr_info("Created early pdomain %u", altp2m_id);
+
+	return altp2m_id;
+
+xmp_early_alloc_free_key:
+	memblock_free_early(__pa(xmp_keys[altp2m_id]), PAGE_SIZE);
+
+xmp_early_alloc_destroy_pdomain:
+	xmp_destroy_pdomain(altp2m_id);
+
+	return XMP_INVALID_PDOMAIN;
+}
+
+static void __init xmp_set_vcpu_enable_notify(struct xmp_vcpu *vcpu)
+{
+        xen_pfn_t gfn = virt_to_pfn(vcpu->vea);
+
+        if (altp2m_set_vcpu_enable_notify(vcpu->nr, gfn)) {
+                xmp_pr_info("Failed to set VEA on vCPU %d", vcpu->nr);
+                return;
+        }
+
+        vcpu->status = XMP_VCPU_STATUS_UP;
+
+        if (xmp_vmfunc(XMP_RESTRICTED_PDOMAIN))
+                xmp_pr_info("Failed to run VMFUNC on vCPU %d", vcpu->nr);
+        else
+                xmp_pr_info("Enabled VMFUNC on vCPU %d (%d/%u)",
+                            vcpu->nr, vcpu->nr + 1, num_possible_cpus());
+}
+
+static void __init xmp_init_vcpu(void *info)
+{
+        struct xmp_vcpu *vcpu = xmp_current_vcpu();
+
+        /*
+         * The first vCPU has already been initialized at this
+         * point. Initialize the other vCPUs here.
+         */
+
+        if (vcpu->status == XMP_VCPU_STATUS_UP)
+                return;
+
+        xmp_set_vcpu_enable_notify(vcpu);
+}
+
+static int __init xmp_init_vcpus(void)
+{
+        return on_each_cpu(xmp_init_vcpu, NULL, 1);
+}
+
+static int __init xmp_alloc_vcpu_vea(void)
+{
+        unsigned int cpu;
+        struct xmp_vcpu *vcpu;
+
+        for_each_possible_cpu(cpu) {
+                vcpu = xmp_vcpu_ptr(cpu);
+
+                vcpu->vea = memblock_virt_alloc_node(PAGE_SIZE, cpu_to_node(cpu));
+                if (!vcpu->vea)
+                        return -ENOMEM;
+
+                vcpu->status = XMP_VCPU_STATUS_DOWN;
+                vcpu->nr = cpu;
+        }
+
+        return 0;
+}
+
+static int __init xmp_alloc_vcpus(void)
+{
+        unsigned int cpus, size;
+
+        cpus = num_possible_cpus();
+        size = sizeof(struct xmp_vcpu) * cpus;
+
+        xmp_vcpu_info.vcpus = memblock_virt_alloc_node(size, NUMA_NO_NODE);
+        if (!xmp_vcpu_info.vcpus)
+                return -ENOMEM;
+
+        xmp_vcpu_info.num_vcpus = cpus;
+
+        return xmp_alloc_vcpu_vea();
+}
+
+static int __init xmp_init_pdomains(void)
+{
+	uint16_t altp2m_id = 0;
+
+	memset(xmp_pdomain_bitmap, 0, sizeof(xmp_pdomain_bitmap));
+	bitmap_set(xmp_pdomain_bitmap, XMP_PRIVILEGED_PDOMAIN, 1);
+
+        if (altp2m_set_domain_state(true)) {
+                xmp_pr_info("Failed to set altp2m state");
+                return -EFAULT;
+        }
+
+	while (altp2m_id < (XMP_MAX_PDOMAINS - 1)) {
+		if (altp2m_create_view(XENMEM_access_default, &altp2m_id))
+			return -EFAULT;
+
+		xmp_pr_info("Created Xen domain %u", altp2m_id);
+	}
+
+
+	/*
+	 * Allocate the first xMP protection domain for the restricted (a.k.a.
+	 * acummulated restrictions) view. This is the view the kernel usually
+	 * resides in.
+	 */
+	xmp_early_alloc_pdomain();
+
+	/*
+	 * Allocate restricted view for page tables (XMP_RESTRICTED_PDOMAIN_PT)
+	 * This view is designed for isolating page tables.
+	 */
+	xmp_early_alloc_pdomain();
+
+        return 0;
+}
+
+int __init xmp_init(void)
+{
+	if (xmp_init_pdomains())
+		return -EFAULT;
+
+	if (xmp_alloc_vcpus())
+		return -EFAULT;
+
+	/*
+	 * To protect as many structures as early as possible, we initialize
+	 * the first vCPU now so that we can isolate them in the early stage
+	 * already.
+	 */
+	xmp_init_vcpu(NULL);
+
+	return 0;
+}
---
+++ include/xen/interface/xmp.h
@@ -0,0 +1,106 @@
+#ifndef __XEN_PUBLIC_XMP_H__
+#define __XEN_PUBLIC_XMP_H__
+
+#include <linux/module.h>
+#include <linux/siphash.h>
+#include <linux/gfp.h>
+
+#include <uapi/linux/const.h>
+
+#include <xen/interface/altp2m.h>
+
+/*
+ * Use this macro instead of VMX_VMFUNC_EPTP_SWITCHING.
+ *
+ * The macro defined in arch/x86/include/asm/vmx.h holds
+ * the wrong value for EPTP switching.
+ */
+#define XMP_VMFUNC_EPTP_SWITCHING       0
+
+/*
+ * XMP_DEBUG
+ */
+
+#ifdef CONFIG_XMP_DEBUG
+#define xmp_pr_info(msg, ...)   pr_info("[ xMP ] %s: " msg "\n", __FUNCTION__, ##__VA_ARGS__)
+#else
+#define xmp_pr_info(msg, ...)
+#endif
+
+/*
+ * xMP domains
+ */
+
+#define XMP_MAX_PDOMAINS	XEN_ALTP2M_MAX_VIEWS
+
+#define XMP_INVALID_PDOMAIN	0xffff
+
+#define XMP_PRIVILEGED_PDOMAIN		0
+#define XMP_RESTRICTED_PDOMAIN		1
+
+#define XMP_RESTRICTED_PDOMAIN_PT	2
+
+/*
+ * xMP allocation flags
+ */
+
+#define XMP_GFP_SHIFT(domain)		(domain) << __GFP_BITS_SHIFT
+#define XMP_GFP_FLAGS(domain, flags)	XMP_GFP_SHIFT(domain) | flags
+#define XMP_GFP_FVIEW(flags)		(flags) >> __GFP_BITS_SHIFT
+
+/*
+ * xMP structures
+ */
+
+typedef enum {
+	XMP_VCPU_STATUS_DOWN,
+	XMP_VCPU_STATUS_UP
+} xmp_vcpu_status_t;
+
+struct vea_struct {
+	uint32_t exit_reason;
+
+	/*
+	 * Indel SDM, Section 25.5.6.1
+	 */
+	uint32_t lock;
+	uint64_t exit_qualification;
+	uint64_t gva;
+	uint64_t gpa;
+	uint16_t altp2m_id;
+};
+
+struct xmp_vcpu {
+	int nr;
+	int status;
+	struct vea_struct *vea;
+};
+
+struct xmp_vcpu_info {
+	unsigned int num_vcpus;
+	struct xmp_vcpu *vcpus;
+};
+
+/*
+ * xMP primitives
+ */
+
+#ifdef CONFIG_XMP
+
+struct xmp_vcpu *xmp_vcpu_ptr(unsigned int cpu);
+
+struct vea_struct *xmp_vcpu_vea(unsigned int cpu);
+
+struct xmp_vcpu *xmp_current_vcpu(void);
+
+struct vea_struct *xmp_current_vea(void);
+
+uint8_t xmp_vmfunc(uint16_t pdomain);
+
+uint16_t xmp_alloc_pdomain(void);
+
+int __init xmp_init(void);
+
+#endif /* CONFIG_XMP */
+
+#endif /* __XEN_PUBLIC_XMP_H__ */
